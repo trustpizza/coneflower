d3c579572978c5ea8eb9ae23284020db1d6838af
Axel <maxelolsson@gmail.com>
2024-06-21 12:31:37 -0400

    Add header

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Development.delete_all
+
+number_of_developments = 7
+
+number_of_developments.times do
+    development = Development.create!(
+        name: Faker::Company.name,
+        address: Faker::Address.full_address,
+        acreage: Faker::Number.decimal_part(digits: 2),  # Example for acreage
+        description: Faker::Lorem.paragraph,
+        website: Faker::Internet.url,
+        planned_start_date: Faker::Date.forward(days: 30),  # Example: 30 days from today
+        planned_end_date: Faker::Date.forward(days: 90)  # Example: 90 days from today
+    )
+end
