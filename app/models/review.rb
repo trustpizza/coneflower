c239cf2692a523c84008eeccc48a85ed2cd5c570
Axel <maxelolsson@gmail.com>
2024-06-25 15:42:19 -0400

    Make a much better frontend for scores

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,12 @@
 class Review < ApplicationRecord
   has_many :scores, dependent: :destroy
 
+  accepts_nested_attributes_for :scores, allow_destroy: true
+
+
   belongs_to :user
   belongs_to :development
+
+  # Validations
+  validates :title, :description, presence: true
 end
