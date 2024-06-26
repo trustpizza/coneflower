e9823ed32528d65272354d63065484fcc4d81971
Axel <maxelolsson@gmail.com>
2024-06-26 10:26:42 -0400

    Railsify images

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,10 +7,10 @@ class SearchController < ApplicationController
       # Search for Developments
       developments = Development.where("LOWER(name) LIKE ? OR LOWER(description) LIKE ?", "%#{@query.downcase}%", "%#{@query.downcase}%")
       # Search for Users
-      users = User.where("LOWER(username) LIKE ?", "%#{@query.downcase}%")
+      # users = User.where("LOWER(username) LIKE ?", "%#{@query.downcase}%")
 
       @results.concat(developments)
-      @results.concat(users)
+      # @results.concat(users)
     end
 
     @results.uniq!  # Remove du
