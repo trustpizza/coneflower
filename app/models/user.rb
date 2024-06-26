4e2d797c777e7e5ec5ad73376f52aa0ec5d78211
Axel <maxelolsson@gmail.com>
2024-06-26 14:31:05 -0400

    Add role enum to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reviews, dependent: :destroy
+
+  enum :role, { user: 0, moderator: 1, admin: 2, owner: 3 }
 end
