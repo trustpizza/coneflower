bbe64a13dc8325b1d3960ea15a33d4762dbbc80c
Axel <maxelolsson@gmail.com>
2024-07-31 12:10:04 -0400

    Add profile update support

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -2,15 +2,19 @@ class Users::ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @profile = current_user.build_profile
+    if current_user.profile
+      redirect_to edit_profile_path(current_user.profile)
+    else
+      @profile = Profile.new
+    end
   end
 
   def create
     @profile = current_user.build_profile(profile_params)
     if @profile.save
-    redirect_to users_profile_path(@profile), notice: 'Profile was successfully created.'
+      redirect_to profile_path(@profile), notice: 'Profile was successfully created.'
     else
-    render :new
+      render :new
     end
   end
 
@@ -21,9 +25,9 @@ class Users::ProfilesController < ApplicationController
   def update
     @profile = current_user.profile
     if @profile.update(profile_params)
-    redirect_to users_profile_path(@profile), notice: 'Profile was successfully updated.'
+      redirect_to profile_path(@profile), notice: 'Profile was successfully updated.'
     else
-    render :edit
+      render :edit
     end
   end
 
