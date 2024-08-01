cdbabd1a846256910d1974431dc6a25fc291298c
Axel <maxelolsson@gmail.com>
2024-08-01 14:55:28 -0400

    Stash

--- app/controllers/users/search_controller.rb
+++ app/controllers/users/search_controller.rb
@@ -1,9 +1,24 @@
 class Users::SearchController < ApplicationController
   def index
     @query = params[:query]
-    @results = []
+    @address = {
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      postal_code: params[:postal_code],
+      country: params[:country]
+    }
 
     if @query.present?
+      # Combine address fields into a single address string
+      @address_parts = [
+        @address[:street_address],
+        @address[:city],
+        @address[:state],
+        @address[:postal_code],
+        @address[:country]
+      ].compact.join(', ')
+
       # Search for Developments
       @pagy, @results = pagy(
         Development.where(
@@ -11,9 +26,23 @@ class Users::SearchController < ApplicationController
         ),
         items: 3
       )
+
+      # If an address is provided, filter by proximity
+      if address_parts.present?
+        geocoded_address = Geocoder.search(address_parts).first
+        if geocoded_address
+          lat, lon = geocoded_address.latitude, geocoded_address.longitude
+          distance = params[:radius] || 50
+          @pagy, @results = pagy(
+            Development.within_distance_of(lat, lon, distance.to_i),
+            items: 3
+          )
+        end
+      end
+
       if !@results.present?
         redirect_to search_index_path, notice: "The following search returned zero results."
       end
-    end 
+    end
   end
 end
